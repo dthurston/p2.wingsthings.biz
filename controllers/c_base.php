c7734c6d8fbcb28fd0f80158b40718c94e5bf456
Derek Thurston <thegrit@gmail.com>
2013-10-23 20:59:57 -0400

    Found an echo statement in controllers/c_users.php that had an echo statement causing a 'headers already sent' error

--- controllers/c_base.php
+++ controllers/c_base.php
@@ -25,6 +25,7 @@ class base_controller {
 		# So we can use $user in views			
 			$this->template->set_global('user', $this->user);
 			
+			
 	}
 	
 } # eoc
--- views/_v_template.php
+++ views/_v_template.php
@@ -11,7 +11,6 @@
 </head>
 
 <body>	
-
 	<?php if(isset($content)) echo $content; ?>
 
 	<?php if(isset($client_files_body)) echo $client_files_body; ?>
