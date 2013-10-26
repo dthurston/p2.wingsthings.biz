d19a4e02f800a50749fc426749b9661c922d691d
Derek Thurston <thegrit@gmail.com>
2013-10-26 12:42:59 -0400

    Added error checking to controller and view

--- controllers/c_users.php
+++ controllers/c_users.php
@@ -40,17 +40,16 @@ class users_controller extends base_controller {
     		echo 'You\'re signed up';          
     }
 
-	public function login() {
+	public function login($error = NULL) {
 
-    	# Setup view
-        	$this->template->content = View::instance('v_users_login');
-        	$this->template->title   = "Login";
+    	# Set up the view
+    	$this->template->content = View::instance("v_users_login");
+        # Pass data to the view
+    	$this->template->content->error = $error;
+        # Render the view
+    	echo $this->template;
 
-    	# Render template
-        	echo $this->template;
-
-	}
-	
+}	
 	public function p_login() {
 
     	# Sanitize the user entered data to prevent any funny-business (re: SQL Injection Attacks)
@@ -67,37 +66,46 @@ class users_controller extends base_controller {
     		AND password = '".$_POST['password']."'";
     		
     		$token = DB::instance(DB_NAME)->select_field($q);
+    	
 
-	    # If we didn't find a matching token in the database, it means login failed
-//    		if(!$token) {
-			if($token == "") {
-        # Send them back to the login page
-	        Router::redirect("/users/login/");
+		# Login failed
+    		if(!$token) {
+    			# Start my new error checking here #
+    			# End adding my new error checking #
+        		# Note the addition of the parameter "error"
+        		$this->template->error = $error;
+                    //echo "user exists, but bad password";
+				# Redirect the user to the error page        		
+        		Router::redirect("/users/login/error"); 
 
-    	# But if we did, login succeeded! 
-    		} else {
-
-        /*
-        Store this token in a cookie using setcookie()
-        Important Note: *Nothing* else can echo to the page before setcookie is called
-        Not even one single white space.
-        param 1 = name of the cookie
-        param 2 = the value of the cookie
-        param 3 = when to expire
-        param 4 = the path of the cooke (a single forward slash sets it for the entire domain)
-        */
-        setcookie("token", $token, strtotime('+1 year'), '/');
-
-        # Send them to the main page - or whever you want them to go
-        Router::redirect("/");
-
+    		}
+    		# Login passed
+    		else {
+        		setcookie("token", $token, strtotime('+2 weeks'), '/');
+        		Router::redirect("/");
     }
 
 }
 
     public function logout() {
-        echo "This is the logout page";
-    }
+
+    # Generate and save a new token for next login
+    $new_token = sha1(TOKEN_SALT.$this->user->email.Utils::generate_random_string());
+
+    # Create the data array we'll use with the update method
+    # In this case, we're only updating one field, so our array only has one entry
+    $data = Array("token" => $new_token);
+
+    # Do the update
+    DB::instance(DB_NAME)->update("users", $data, "WHERE token = '".$this->user->token."'");
+
+    # Delete their token cookie by setting it to a date in the past - effectively logging them out
+    setcookie("token", "", strtotime('-1 year'), '/');
+
+    # Send them back to the main index.
+    Router::redirect("/");
+
+}
 
     public function profile($user_name = NULL) {
 
